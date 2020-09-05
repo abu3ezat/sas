5005291683c04785428399514fe9d0a4fc2f44b3
abu3ezat <abu3ezat@gmail.com>
2020-09-05 14:55:26 +0300

    Updated

--- js/app.js
+++ js/app.js
@@ -18,10 +18,39 @@ for (let i = 0 ; i<cards.length; i++){
 }
 
 let btn = document.querySelectorAll(".select-button");
+let [x , y , z] = document.querySelectorAll(".feature-box");
 for (let i = 0 ; i<btn.length; i++){
 
     btn[i].onclick = function () {
         for (let j =0; j< btn.length; j++) btn[j].classList.remove("butt-active")
         btn[i].classList.add("butt-active");
+        if (i === 0){
+            x.style.display = "flex";
+            y.style.display = "none";
+            z.style.display = "none";
+        }
+        if (i === 1){
+            y.style.display = "flex";
+            x.style.display = "none";
+            z.style.display = "none";
+        }
+        if (i === 2){
+            z.style.display = "flex";
+            x.style.display = "none";
+            y.style.display = "none";
+        }
     }
 }
+
+// let btns = document.querySelectorAll(".select-button");
+// let boxes = document.querySelectorAll(".feature-box");
+// btns.forEach(function (btn, key) {
+//     btn.onclick = function () {
+//         btns.classList.remove("butt-active");
+//         btns[key].classList.add("butt-active");
+//         boxes.forEach(function (box, key) {
+//
+//         })
+//         btns[key].classList.add("butt-active");
+//     }
+// })
